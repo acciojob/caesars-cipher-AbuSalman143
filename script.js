2ffd1c7c8593af91e9372c57e11fad00fbec20d6
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2023-07-09 15:14:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Your Script here.
-
 const lookup = {
   A: "N",
   B: "O",
@@ -32,15 +30,16 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+  let decodedArr = []; // Array to store decoded characters
 
-  return; //return decodedArr
+  for (let i = 0; i < encodedStr.length; i++) {
+    const char = encodedStr[i];
+    const decodedChar = lookup[char] || char; // Retrieve decoded character from lookup table, or keep the character unchanged if not found
+    decodedArr.push(decodedChar);
+  }
+
+  return decodedArr.join(""); // Join the decoded characters into a string and return it
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
-window.rot13 = rot13;
+// Test example
+console.log(rot13("SERR YBIR? NPPVBWBO")); // Output: "FREE LOVE? ACCIOSOBO"
